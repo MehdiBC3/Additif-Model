c16f5dabd1261785a42a374370ccf481e3429ddd
MehdiBC3 <156785256+MehdiBC3@users.noreply.github.com>
2024-01-31 15:21:21 +0100

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from data_preparation import DataPreparation
-from regression import Regression
+from Additif import Regression
 
 
-csv_path = "./number of travelers.csv"
+csv_path = "./vente_maillots_de_bain.csv"
 data_preparation_object = DataPreparation(csv_path)
 regression_object = Regression(data_preparation_object)
+data_preparation_object.afficher_dataframe()
 
 # data_preparation_object.show_graph()
